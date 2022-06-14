879236ae79b021bee7c7809e2839d78344f7c325
vivid <vivid022900@163.com>
2022-06-14 21:08:12 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,14 @@
+package com.atguigu.git;
+
+/**
+ * @Author CZQ
+ * @Date 2022/6/14 19:22
+ * @Version 1.0
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello,git");
+    }
+}
+
+
